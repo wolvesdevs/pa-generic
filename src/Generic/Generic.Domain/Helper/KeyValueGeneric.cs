c0ec010a541124291a43744b65f1e435aeea4b18
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-27 11:21:47 +0900

    refs #009 型パラメータを複数の場所で使う

--- src/Generic/Generic.Domain/Helper/KeyValueGeneric.cs
+++ src/Generic/Generic.Domain/Helper/KeyValueGeneric.cs
@@ -2,11 +2,27 @@
 
 public class KeyValueGeneric<T>
 {
+    private T _value;
+    private List<T> _values = new();
+
     public int Key { get; set; }
     public T Value { get; set; }
+    public T Value2 { get; set; }
+
+    public event Action<T> MyAction;
 
     public override string ToString()
     {
         return $"ID = {Key}, Value = {Value}";
+    }
+
+    public void Add(T value)
+    {
+        _values.Add(value);
+    }
+
+    public T GetValue(int index)
+    {
+        return _values[index];
     }
 }
