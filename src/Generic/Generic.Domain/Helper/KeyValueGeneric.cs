5d2890f0f1b8cf0212c96af2108eee2434c6a022
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-27 09:44:57 +0900

    refs #005 ジェネリック型の作り方

---
+++ src/Generic/Generic.Domain/Helper/KeyValueGeneric.cs
@@ -0,0 +1,12 @@
+﻿namespace Generic.Domain.Helper;
+
+public class KeyValueGeneric<T>
+{
+    public int Key { get; set; }
+    public T Value { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID = {Key}, Value = {Value}";
+    }
+}
