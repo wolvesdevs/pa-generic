f9d90c822fc07e63b5c4f3814285b41df405ad3a
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-28 10:11:06 +0900

    refs #013 制約をしていない場合のnullチェックの挙動

--- src/Generic/Generic.UI/Views/SubView02.cs
+++ src/Generic/Generic.UI/Views/SubView02.cs
@@ -15,7 +15,14 @@ public partial class SubView02 : Form
         KeyValueA<int> keyValueA = new();
 
         // 参照型制約
-        KeyValueB<string> keyValueB = new();
-        KeyValueB<Product> keyValueC = new();
+        KeyValueB<string> keyValueB1 = new();
+        KeyValueB<Product> keyValueB2 = new();
+
+        KeyValueC<string> c1 = new();
+        c1.Run();
+
+        KeyValueC<int> c2 = new();
+        c2.Run();
+
     }
 }
