a7d40bb37752306c8da6c66c9036c3afaeb0032c
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-02-29 09:54:05 +0900

    refs #019 インターフェイス制約の効果

--- src/Generic/Generic.Domain/Helper/ClassF.cs
+++ src/Generic/Generic.Domain/Helper/ClassF.cs
@@ -4,5 +4,16 @@ namespace Generic.Domain.Helper;
 
 public class ClassF<T> where T : class, IEntity, IDisposable
 {
+    private T _value;
+    private List<T> _entities = new();
 
+    public T? Find(int id)
+    {
+        return _entities.Where(x => x.Id == id).FirstOrDefault();
+    }
+
+    public void Add()
+    {
+        _value.Id = 1;
+    }
 }
