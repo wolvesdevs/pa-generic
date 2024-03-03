0e75482dcfcc75cdbd9c60bac500ae2250a0e5c9
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-03 13:24:16 +0900

    refs #026 ゼロ系の値_default(T)

--- src/Generic/Generic.UI/ViewModels/ClientViewModel.cs
+++ src/Generic/Generic.UI/ViewModels/ClientViewModel.cs
@@ -12,14 +12,18 @@ public class ClientViewModel
 
         //ClassG<StockEntity> classG2 = new();
 
-        ClassH<int> k1 = new();
-        ClassH<Point> k2 = new();
-        ClassH<string> k3 = new();
+        //ClassH<int> k1 = new();
+        //ClassH<Point> k2 = new();
+        //ClassH<string> k3 = new();
 
-        ClassH<int?> k11 = new();
-        ClassH<Point?> k12 = new();
-        ClassH<string?> k13 = new();
+        //ClassH<int?> k11 = new();
+        //ClassH<Point?> k12 = new();
+        //ClassH<string?> k13 = new();
 
-        ClassK<ProductEntity, Form> k4 = new();
+        //ClassK<ProductEntity, Form> k4 = new();
+
+        var l1 = new ClassL<int>().GetData();
+        var l2 = new ClassL<bool>().GetData();
+        var l3 = new ClassL<string>().GetData();
     }
 }
