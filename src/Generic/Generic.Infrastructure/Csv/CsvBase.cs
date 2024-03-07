2223939af5d5b22889f35a74241baca490a33681
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-07 10:34:14 +0900

    refs #036 ジェネリックの抽象クラスを作る

---
+++ src/Generic/Generic.Infrastructure/Csv/CsvBase.cs
@@ -0,0 +1,40 @@
+﻿using Generic.Domain.Entities;
+using Generic.Domain.Exceptions;
+
+namespace Generic.Infrastructure.Csv
+{
+    public abstract class CsvBase<T>
+    {
+        public IEnumerable<T> GetAll()
+        {
+            var lines = File.ReadAllLines(GetPath());
+            bool isFirst = true;
+            List<T> entities = new();
+
+            foreach (var line in lines)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    continue;
+                }
+
+                var items = line.Split(',');
+                if (items.Length != GetItemCount())
+                {
+                    throw new CsvException();
+                }
+
+                var product = GetEntity(items);
+
+                entities.Add(product);
+            }
+
+            return entities;
+        }
+
+        protected abstract string GetPath();
+        protected abstract int GetItemCount();
+        protected abstract T GetEntity(string[] items);
+    }
+}
