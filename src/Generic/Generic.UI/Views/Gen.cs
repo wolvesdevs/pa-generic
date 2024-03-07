ffd6cb69c3d4eddd7b86341580dd806b5d2bcf6e
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-07 10:43:30 +0900

    refs #037 サブクラスの実装

--- src/Generic/Generic.UI/Views/Gen.cs
+++ src/Generic/Generic.UI/Views/Gen.cs
@@ -1,5 +1,6 @@
 ﻿using Generic.Domain.Entities;
 using Generic.Domain.Exceptions;
+using Generic.Infrastructure.Csv;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,57 +22,13 @@ public partial class Gen : Form
 
     private void button1_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Product.csv");
-        bool isFirst = true;
-        List<ProductEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 3)
-            {
-                throw new CsvException();
-            }
-
-            ProductEntity product = new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
-
-            entities.Add(product);
-        }
-
-        dataGridView1.DataSource = entities;
+        ProductCsv productCsv = new();
+        dataGridView1.DataSource = productCsv.GetAll();
     }
 
     private void button2_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Stock.csv");
-        bool isFirst = true;
-        List<StockEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 2)
-            {
-                throw new CsvException();
-            }
-
-            StockEntity product = new(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
-
-            entities.Add(product);
-        }
-
-        dataGridView1.DataSource = entities;
+        StockCsv stockCsv = new();
+        dataGridView1.DataSource = stockCsv.GetAll();
     }
 }
