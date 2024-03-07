98488d9d8d22f66dc64bfca5e6c36ea34f5939be
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-07 15:09:00 +0900

    refs #039 ジェネリックメソッドに変更する

---
+++ src/Generic/Generic.Infrastructure/Csv/CsvHelper.cs
@@ -0,0 +1,34 @@
+﻿using Generic.Domain.Exceptions;
+
+namespace Generic.Infrastructure.Csv;
+
+public static class CsvHelper
+{
+    public static IEnumerable<T> GetAll<T>(string path, int itemCount, Func<string[], T> func)
+    {
+        var lines = File.ReadAllLines(path);
+        bool isFirst = true;
+        List<T> entities = new();
+
+        foreach (var line in lines)
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                continue;
+            }
+
+            var items = line.Split(',');
+            if (items.Length != itemCount)
+            {
+                throw new CsvException();
+            }
+
+            var entity = func(items);
+
+            entities.Add(entity);
+        }
+
+        return entities;
+    }
+}
