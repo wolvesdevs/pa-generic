f83845ce15278b5b5c1a83df4bce8f17f430332b
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-08 10:16:30 +0900

    refs #040 outキーワードの実装_IRead<out T>

--- src/Generic/Generic.UI/Program.cs
+++ src/Generic/Generic.UI/Program.cs
@@ -14,3 +14,45 @@ internal static class Program
         Application.Run(new Gen());
     }
 }
+
+public interface IRead<out T>
+{
+    T GetData(int index);
+}
+
+public class Entities<T> : IRead<T>
+{
+    private List<T> _entities = new();
+
+    public T GetData(int index)
+    {
+        return _entities[index];
+    }
+
+    public void Add(T entity)
+    {
+        _entities.Add(entity);
+    }
+}
+
+public class Use
+{
+    public static void UseA()
+    {
+        Entities<Product> products = new();
+        products.Add(new Product(1, "AAA"));
+        Read(products);
+    }
+
+    private static void Read(IRead<IEntity> entities)
+    {
+        var val = entities.GetData(0);
+    }
+}
+
+public interface IEntity { }
+public class Product(int id, string name) : IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
