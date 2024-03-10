1fd346a746b0ec4d6463e6e1e58a4206fdca42ca
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-10 15:25:07 +0900

    refs #043 【復習】ジェネリックの抽象クラスの作成＆サブクラスの実装

--- src/Generic/Generic.Infrastructure/Csv/StockCsv.cs
+++ src/Generic/Generic.Infrastructure/Csv/StockCsv.cs
@@ -4,17 +4,17 @@ namespace Generic.Infrastructure.Csv;
 
 public class StockCsv : CsvBase<StockEntity>
 {
-    protected override StockEntity GetEntity(string[] items)
+    public override StockEntity GetEntity(string[] items)
     {
         return new StockEntity(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
     }
 
-    protected override int GetItemCount()
+    public override int GetItemLength()
     {
         return 2;
     }
 
-    protected override string GetPath()
+    public override string GetPath()
     {
         return "Stock.csv";
     }
