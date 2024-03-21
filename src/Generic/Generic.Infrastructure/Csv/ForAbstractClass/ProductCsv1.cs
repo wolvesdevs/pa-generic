253987cb415d7f11f740264ab3e87846a2ce6b23
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-21 09:53:12 +0900

    refs #057 練習用ファイル修正

--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
@@ -2,15 +2,9 @@
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class ProductCsv1 : CsvBase<ProductEntity>
+public class ProductCsv1
 {
-    public override string FilePath => "Product.csv";
-    public override int ItemLength => 3;
 
-    public override ProductEntity GetEntity(string[] items)
-    {
-        return new ProductEntity(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
-    }
 }
 
 //public class ProductCsv1 : CsvBase<ProductEntity>
--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
@@ -2,15 +2,9 @@
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class StockCsv1 : CsvBase<StockEntity>
+public class StockCsv1
 {
-    public override string FilePath => "Stock.csv";
-    public override int ItemLength => 2;
 
-    public override StockEntity GetEntity(string[] items)
-    {
-        return new StockEntity(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
-    }
 }
 
 //public class StockCsv1 : CsvBase<StockEntity>
