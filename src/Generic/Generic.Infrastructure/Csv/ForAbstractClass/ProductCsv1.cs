464be7f90588fcd9b6bde93a618024920c13071e
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-23 13:00:18 +0900

    refs #058 【練習】ジェネリックの抽象クラスに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
@@ -2,9 +2,15 @@
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class ProductCsv1
+public class ProductCsv1 : CsvBase<ProductEntity>
 {
+    public override string FilePath => "Product.csv";
+    public override int ItemCount => 3;
 
+    public override ProductEntity GetEntity(string[] items)
+    {
+        return new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
+    }
 }
 
 //public class ProductCsv1 : CsvBase<ProductEntity>
