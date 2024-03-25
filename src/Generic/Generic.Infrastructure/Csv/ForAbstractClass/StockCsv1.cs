6bd644fbaa06e44531caacf196a54e245106fe8e
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-25 16:24:00 +0900

    refs #060 【練習】ジェネリックの抽象クラスに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
@@ -2,9 +2,15 @@
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class StockCsv1
+public class StockCsv1 : CsvBase<StockEntity>
 {
+    public override string FilePath => "Stock.csv";
+    public override int ItemCount => 2;
 
+    public override StockEntity GetEntity(string[] items)
+    {
+        return new(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
+    }
 }
 
 //public class StockCsv1 : CsvBase<StockEntity>
--- src/Generic/Generic.UI/Views/Gen.cs
+++ src/Generic/Generic.UI/Views/Gen.cs
@@ -30,32 +30,8 @@ public partial class Gen : Form
     /// <exception cref="CsvException"></exception>
     private void ProductCsv1Button_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Product.csv");
-        bool isFirst = true;
-        List<ProductEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 3)
-            {
-                throw new CsvException();
-            }
-
-            ProductEntity product = new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
-
-            entities.Add(product);
-        }
-
-        dataGridView1.DataSource = entities;
-
-
+        ProductCsv1 productCsv1 = new();
+        dataGridView1.DataSource = productCsv1.GetAll();
     }
 
     /// <summary>
@@ -66,32 +42,8 @@ public partial class Gen : Form
     /// <exception cref="CsvException"></exception>
     private void StockCsv1Button_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Stock.csv");
-        bool isFirst = true;
-        List<StockEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 2)
-            {
-                throw new CsvException();
-            }
-
-            StockEntity product = new(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
-
-            entities.Add(product);
-        }
-
-        dataGridView1.DataSource = entities;
-
-
+        StockCsv1 stockCsv1 = new();
+        dataGridView1.DataSource = stockCsv1.GetAll();
     }
 
     /// <summary>
