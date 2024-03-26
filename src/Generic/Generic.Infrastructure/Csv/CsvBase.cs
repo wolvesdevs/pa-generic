62edafe894afbc6e565e00f399705a66f5246c91
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-27 08:39:39 +0900

    refs #063 【練習】ジェネリックの抽象クラスに置き換える

--- src/Generic/Generic.Infrastructure/Csv/CsvBase.cs
+++ src/Generic/Generic.Infrastructure/Csv/CsvBase.cs
@@ -1,10 +1,45 @@
 ﻿using Generic.Domain.Entities;
 using Generic.Domain.Exceptions;
 using Generic.Domain.Repositories;
+using System.Runtime.CompilerServices;
 
 namespace Generic.Infrastructure.Csv;
 
+public abstract class CsvBase<T> where T : IEntity
+{
+    public abstract string FilePath { get; }
+    public abstract int ItemCount { get; }
 
+    public object GetAll()
+    {
+        var lines = File.ReadAllLines(FilePath);
+        bool isFirst = true;
+        List<T> entities = new();
+
+        foreach (var line in lines)
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                continue;
+            }
+
+            var items = line.Split(',');
+            if (items.Length != ItemCount)
+            {
+                throw new CsvException("無効なファイルフォーマットです。");
+            }
+
+            var entity = GetEntity(items);
+
+            entities.Add(entity);
+        }
+
+        return entities;
+    }
+
+    public abstract T GetEntity(string[] items);
+}
 
 //public abstract class CsvBase<T> where T : IEntity
 //{
