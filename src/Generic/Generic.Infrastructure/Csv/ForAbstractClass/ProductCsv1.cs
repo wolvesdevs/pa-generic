7899621c11703a7a658475cc14d6a218b0d100b6
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-03-29 09:23:49 +0900

    refs #065 【練習】ジェネリックの抽象クラスに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
@@ -3,9 +3,15 @@ using Generic.Domain.Exceptions;
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class ProductCsv1
+public class ProductCsv1 : CsvBase<ProductEntity>
 {
+    public override string FilePath => "Product.csv";
+    public override int ItemCount => 3;
 
+    public override ProductEntity GetEntity(string[] items)
+    {
+        return new ProductEntity(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
+    }
 }
 
 //public class ProductCsv1 : CsvBase<ProductEntity>
--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/StockCsv1.cs
@@ -3,9 +3,15 @@ using Generic.Domain.Exceptions;
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
 
-public class StockCsv1
+public class StockCsv1 : CsvBase<StockEntity>
 {
+    public override string FilePath => "Stock.csv";
+    public override int ItemCount => 2;
 
+    public override StockEntity GetEntity(string[] items)
+    {
+        return new StockEntity(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
+    }
 }
 
 //public class StockCsv1 : CsvBase<StockEntity>
