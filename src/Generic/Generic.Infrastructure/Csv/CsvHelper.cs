00bd80365f6313270e8b0512b1a944ba1d4870a3
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-04-03 09:11:33 +0900

    refs #070 【練習】ジェネリックメソッドに置き換える

--- src/Generic/Generic.Infrastructure/Csv/CsvHelper.cs
+++ src/Generic/Generic.Infrastructure/Csv/CsvHelper.cs
@@ -5,7 +5,33 @@ namespace Generic.Infrastructure.Csv;
 
 public static class CsvHelper
 {
+    public static IEnumerable<T> GetAll<T>(string filePath, int itemCount, Func<string[], T> func)
+    {
+        var lines = File.ReadAllLines(filePath);
+        bool isFirst = true;
+        List<T> entities = new();
 
+        foreach (var line in lines)
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                continue;
+            }
+
+            var items = line.Split(',');
+            if (items.Length != itemCount)
+            {
+                throw new CsvException("ファイルフォーマットが無効です。");
+            }
+
+            var entity = func(items);
+
+            entities.Add(entity);
+        }
+
+        return entities;
+    }
 
     //public static IEnumerable<T> GetAll<T>(string path, int itemCount, Func<string[], T> func)
     //{
