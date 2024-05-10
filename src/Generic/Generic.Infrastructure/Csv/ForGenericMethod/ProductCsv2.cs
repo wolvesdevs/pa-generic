f36552d2d2116b7c67629b68c4b9769958cdf3a7
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-05-10 09:23:31 +0900

    refs #106 【練習】ジェネリックメソッドに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForGenericMethod/ProductCsv2.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForGenericMethod/ProductCsv2.cs
@@ -5,8 +5,34 @@ namespace Generic.Infrastructure.Csv.ForGenericMethod;
 
 public class ProductCsv2
 {
+    public IEnumerable<ProductEntity> GetAll()
+    {
+        var lines = File.ReadAllLines("Product.csv");
+        bool isFirst = true;
+        List<ProductEntity> entities = new();
 
-    
+        foreach (var line in lines)
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                continue;
+            }
+
+            var items = line.Split(',');
+            if (items.Length != 3)
+            {
+                throw new CsvException("ファイルフォーマットが無効です。");
+            }
+
+            ProductEntity product = new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
+
+            entities.Add(product);
+        }
+
+        return entities;
+    }
+
     //public IEnumerable<ProductEntity> GetAll()
     //{
     //    return CsvHelper.GetAll("Product.csv", 3, items => new ProductEntity(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2])));
