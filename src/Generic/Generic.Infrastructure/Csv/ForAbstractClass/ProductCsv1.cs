0aeb59a5a942617e91ca1acce1c871067d5bf086
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-05-25 14:45:53 +0900

    refs #121 【練習】ジェネリックの抽象クラスに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForAbstractClass/ProductCsv1.cs
@@ -2,10 +2,15 @@
 using Generic.Domain.Exceptions;
 
 namespace Generic.Infrastructure.Csv.ForAbstractClass;
-
-public class ProductCsv1
+public class ProductCsv1 : CsvBase<ProductEntity>
 {
+    public override string FilePath => "Product.csv";
+    public override int ItemCount => 3;
 
+    public override ProductEntity GetEntity(string[] items)
+    {
+        return new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
+    }
 }
 
 //public class ProductCsv1 : CsvBase<ProductEntity>
--- src/Generic/Generic.UI/Views/Gen.cs
+++ src/Generic/Generic.UI/Views/Gen.cs
@@ -1,17 +1,6 @@
 ﻿using Generic.Domain.Entities;
 using Generic.Domain.Exceptions;
-using Generic.Infrastructure.Csv;
 using Generic.Infrastructure.Csv.ForAbstractClass;
-using Generic.Infrastructure.Csv.ForGenericMethod;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Generic.UI.Views;
 public partial class Gen : Form
@@ -30,32 +19,8 @@ public partial class Gen : Form
     /// <exception cref="CsvException"></exception>
     private void ProductCsv1Button_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Product.csv");
-        bool isFirst = true;
-        List<ProductEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 3)
-            {
-                throw new CsvException("ファイルフォーマットが無効です。");
-            }
-
-            ProductEntity product = new(Convert.ToInt32(items[0]), items[1], Convert.ToInt32(items[2]));
-
-            entities.Add(product);
-        }
-
-        dataGridView1.DataSource = entities;
-
-
+        ProductCsv1 productCsv1 = new();
+        dataGridView1.DataSource = productCsv1.GetAll();
     }
 
     /// <summary>
@@ -66,32 +31,8 @@ public partial class Gen : Form
     /// <exception cref="CsvException"></exception>
     private void StockCsv1Button_Click(object sender, EventArgs e)
     {
-        var lines = File.ReadAllLines("Stock.csv");
-        bool isFirst = true;
-        List<StockEntity> entities = new();
-
-        foreach (var line in lines)
-        {
-            if (isFirst)
-            {
-                isFirst = false;
-                continue;
-            }
-
-            var items = line.Split(',');
-            if (items.Length != 2)
-            {
-                throw new CsvException("ファイルフォーマットが無効です。");
-            }
-
-            StockEntity stock = new(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]));
-
-            entities.Add(stock);
-        }
-
-        dataGridView1.DataSource = entities;
-
-
+        StockCsv1 stockCsv1 = new();
+        dataGridView1.DataSource = stockCsv1.GetAll();
     }
 
     /// <summary>
