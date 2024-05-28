3697fd9ebef43255e991afa6df5b531d9b02ffef
wolvesdevs_work <75551104+wolvesdevs@users.noreply.github.com>
2024-05-28 22:23:48 +0900

    refs #124 【練習】ジェネリックメソッドに置き換える

--- src/Generic/Generic.Infrastructure/Csv/ForGenericMethod/StockCsv2.cs
+++ src/Generic/Generic.Infrastructure/Csv/ForGenericMethod/StockCsv2.cs
@@ -4,7 +4,10 @@ using Generic.Domain.Exceptions;
 namespace Generic.Infrastructure.Csv.ForGenericMethod;
 public class StockCsv2
 {
-
+    public IEnumerable<StockEntity> GetAll()
+    {
+        return CsvHelper.GetAll("Stock.csv", 2, items => new StockEntity(Convert.ToInt32(items[0]), Convert.ToInt32(items[1])));
+    }
 
     //public IEnumerable<StockEntity> GetAll()
     //{
